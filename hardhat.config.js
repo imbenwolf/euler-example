cf2cd88b37a5b245d37444be3597bc728f161d87
Ben Wolf <benjamineliaswolf@gmail.com>
2022-06-28 18:25:11 +0200

    Update hardhat network to fork from alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/8pn0QO-8qJ2iSMxKngRLcye4NT8z4tLu",
+      },
+    },
+  },
 };
